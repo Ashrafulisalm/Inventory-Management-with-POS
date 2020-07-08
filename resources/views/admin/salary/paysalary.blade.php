d0afed196e52db84f704fc8bb49b7329eea86941
Ashraful Islam <59199868+Ashrafulisalm@users.noreply.github.com>
2020-07-08 23:20:59 +0600

    expense Complete

--- resources/views/admin/salary/paysalary.blade.php
+++ resources/views/admin/salary/paysalary.blade.php
@@ -17,6 +17,7 @@
                               <th scope="col">Photo</th>
                               <th scope="col">Salary</th>
                               <th scope="col">Advanced</th>
+                              <th scope="col">Pay Amount</th>
                               <th scope="col">Month</th>
                               <th scope="col">Action</th>
                               
@@ -29,12 +30,15 @@
                               <td><img src="{{asset($row->photo)}}" height="50" width="50"></td>
                               <td>{{$row->salary}}</td>
                               <?php $ad=App\Advancedsalary::where('employee_id',$row->id)->where('month',date("F", strtotime("-1 months")))->first();
+                                
                                 if($ad){
+                                  
                                ?>
                               <td>{{$ad->advanced_salary}}</td>
                               <?php }else { ?>
                                 <td></td>
                               <?php } ?>
+                              <td></td>
                               <td>{{date("F", strtotime("-1 months"))}}</td>
                               <td><a class="btn btn-primary" href="">Pay Salary</a>
                               
--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -98,67 +98,6 @@
         </div>
       </li>
 
-
-     <li class="nav-item active">
-          <a class="nav-link fas fa-fw fa-user-lock" href="{{ route('logout') }}"
-             onclick="event.preventDefault();
-                           document.getElementById('logout-form').submit();">
-              {{  __('Logout') }}
-          </a>
-
-          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-              @csrf
-          </form>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Interface
-      </div>
-
-      <!-- Nav Item - Pages Collapse Menu -->
-      <!-- <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-        </div>
-      </li> -->
-
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <!-- <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
-          </div>
-        </div>
-      </li> -->
-
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Addons
-      </div>
-
       <!-- Nav Item - Pages Collapse Menu -->
       <li class="nav-item">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
@@ -179,18 +118,17 @@
         </div>
       </li>
 
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
 
-      <!-- Nav Item - Tables -->
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
+     <li class="nav-item active">
+          <a class="nav-link fas fa-fw fa-user-lock" href="{{ route('logout') }}"
+             onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+              {{  __('Logout') }}
+          </a>
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
       </li>
 
       <!-- Divider -->
